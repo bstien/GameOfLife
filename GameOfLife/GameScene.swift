e9221974b33f20a8df3190ee0e24c6fe9a9cf897
Bastian Stien <bastian.stien@gmail.com>
2022-11-26 18:14:25 +0100

    Core implementation

--- GameOfLife/GameScene.swift
+++ GameOfLife/GameScene.swift
@@ -3,6 +3,13 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+    // MARK: - Private properties
+
+    private var columns = 0
+    private var rows = 0
+    private var cellNodes = [[CellNode]]()
+    private var livingCells = Set<LivingCell>()
+
     // MARK: - Lifecycle
 
     override func didMove(to view: SKView) {
@@ -10,42 +17,101 @@ class GameScene: SKScene {
         backgroundColor = .secondarySystemBackground
 
         let viewPort = view.bounds.inset(by: view.safeAreaInsets)
-        let cellSize: CGFloat = 8
+        let cellSize: CGFloat = 5
         let padding: CGFloat = 1
 
-        let numCols = Int(floor(viewPort.width / cellSize))
-        let numRows = Int(floor(viewPort.height / cellSize))
+        columns = Int(floor(viewPort.width / cellSize))
+        rows = Int(floor(viewPort.height / cellSize))
 
         let colOuterPadding = viewPort.width.truncatingRemainder(dividingBy: cellSize) / 2
         let rowOuterPadding = viewPort.height.truncatingRemainder(dividingBy: cellSize) / 2
 
-        for column in (0..<numCols) {
-            for row in (0..<numRows) {
-                let cell = SKSpriteNode()
+        cellNodes = Array(repeating: [], count: columns)
+        for column in (0..<columns) {
+            for row in (0..<rows) {
+                let cell = CellNode()
                 cell.anchorPoint = .zero
-                cell.color = .secondaryLabel
+                cell.color = .black
 
                 let sideSize = cellSize - padding
                 cell.size = CGSize(width: sideSize, height: sideSize)
+                cell.position = CGPoint(
+                    x: (CGFloat(column) * cellSize) + colOuterPadding,
+                    y: (CGFloat(row) * cellSize) + rowOuterPadding
+                )
 
-                let xPos: CGFloat = (CGFloat(column) * cellSize) + colOuterPadding
-                let yPos: CGFloat = (CGFloat(row) * cellSize) + rowOuterPadding
-                cell.position = CGPoint(x: xPos, y: yPos)
-
+                cellNodes[column].append(cell)
                 addChild(cell)
             }
         }
+
+        let livingCells = createInitialCells(columnCount: columns, rowCount: rows, fillPercentage: 30)
+        updateLivingCells(newCells: livingCells)
     }
 
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        super.update(currentTime)
+
+        var newLivingCells = Set<LivingCell>()
+
+        for column in (0..<columns) {
+            for row in (0..<rows) {
+                let cell = cellNodes[column][row]
+                let livingNeighbors = countLivingNeighbors(column: column, row: row)
+
+                if cell.isLiving, [2, 3].contains(livingNeighbors) {
+                    newLivingCells.insert(LivingCell(x: column, y: row))
+                } else if !cell.isLiving, livingNeighbors == 3 {
+                    newLivingCells.insert(LivingCell(x: column, y: row))
+                }
+            }
+        }
+
+        updateLivingCells(newCells: newLivingCells)
     }
 
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        touches.forEach {
-            let location = $0.location(in: self)
-            let node = atPoint(location) as? SKSpriteNode
-            node?.color = .red
+    // MARK: - Private methods
+
+    private func countLivingNeighbors(column: Int, row: Int) -> Int {
+        var livingNeighbors = 0
+        for x in (-1...1) {
+            for y in (-1...1) {
+                // Don't count self.
+                if x == 0, y == 0 { continue }
+
+                if livingCells.contains(LivingCell(x: column + x, y: row + y)) {
+                    livingNeighbors += 1
+                }
+            }
         }
+        return livingNeighbors
+    }
+
+    private func updateLivingCells(newCells: Set<LivingCell>) {
+        let dyingCells = livingCells.subtracting(newCells)
+        let bornCells = newCells.subtracting(livingCells)
+        livingCells = newCells
+
+        for dyingCell in dyingCells {
+            cellNodes[dyingCell.x][dyingCell.y].isLiving = false
+        }
+
+        for bornCell in bornCells {
+            cellNodes[bornCell.x][bornCell.y].isLiving = true
+        }
+    }
+
+    private func createInitialCells(columnCount: Int, rowCount: Int, fillPercentage: Int) -> Set<LivingCell> {
+        let totalCells = columnCount * rowCount
+        let numberOfLivingCells = Int(Double(totalCells) * Double(fillPercentage) / 100)
+
+        var livingCells = Set<LivingCell>()
+        repeat {
+            let xPos = Int.random(in: 0..<columnCount)
+            let yPos = Int.random(in: 0..<rowCount)
+            livingCells.insert(LivingCell(x: xPos, y: yPos))
+        } while (livingCells.count <= numberOfLivingCells)
+
+        return livingCells
     }
 }
